3cddd65062cd84426c5f96a4aab1612b7b4e794e
kilocharlie96 <cernak.kiko@gmail.com>
2023-04-26 10:37:09 +0200

    Content flexbox created

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
 (function($){
-    var header = $('header');
-    header.hide().fadeIn(2000);
+    var body = $('body'),
+    list = $('.work-experience'),
+    copy = $('.copy');
+
+
+    //skryjem elementy dd
+list.find('dd').slideUp();
+
+//zobrazim dd po kliknuti na dt a ostatne dd schovam
+list.find('dt').on('click', function()
+{
+    var dt = $(this),
+        dd = dt.next();    
+    
+    dd.slideToggle()
+    .siblings('dd').slideUp();
+
+    var newContent = dt.text() + dd.html();
+
+    //skopirujem z dt a dd do copy
+    copy.append(newContent + '<hr>');
+
+    event.preventDefault();
+});
+    body.hide().fadeIn(2000);
 })(jQuery);
